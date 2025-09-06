abcc91f35c9ac9845296d3ef89e0f6a2066060c5
Jondahl Rhenius <jondahlrh@gmail.com>
2025-09-06 07:18:54 +0200

    Add route to get command

---
+++ src/routes/faceit/player/getCommand.ts
@@ -0,0 +1,49 @@
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod/v4";
+
+import type FaceitApiService from "../../../services/faceitApiService/FaceitApiService.js";
+
+type Config = {
+  faceitApiService: FaceitApiService;
+};
+
+export default function getCommand(
+  app: FastifyInstance,
+  options: { config: Config },
+) {
+  const { faceitApiService } = options.config;
+
+  app.withTypeProvider<ZodTypeProvider>().route({
+    method: "GET",
+    url: "",
+    schema: {
+      params: z.object({ id: z.string() }),
+      querystring: z.object({ bot: z.string().optional() }),
+      response: { 200: z.any() },
+    },
+    handler: async (req, res) => {
+      const { bot = "nightbot" } = req.query;
+
+      const player = await faceitApiService.getPlayer(req.params.id);
+      if (player === undefined || player.player_id === undefined) {
+        return res.code(404).send("player not found");
+      }
+
+      const fullUrlWithoutPath = `${req.protocol}://${req.host}`;
+
+      if (bot === "nightbot") {
+        let response = "Bot: nightbot\n\n";
+        response += `Add new Command:       !addcom !elo $(urlfetch ${fullUrlWithoutPath}/faceit/player/${player.player_id}/elo?nickname=$(querystring))\n`;
+        response += `Edit existing Command: !editcom !elo $(urlfetch ${fullUrlWithoutPath}/faceit/player/${player.player_id}/elo?nickname=$(querystring))\n`;
+        return res.code(200).send(response);
+      }
+
+      let response = "Bot not found, available bot(s): nightbot\n\n";
+      response += `Url to be used:                ${fullUrlWithoutPath}/faceit/player/${player.player_id}/elo\n`;
+      response += `Url with optional querystring: ${fullUrlWithoutPath}/faceit/player/${player.player_id}/elo?nickname=<optional_querystring>\n\n`;
+      response += `=> replace "<optional_querystring>" with bot specific variable\n`;
+      return res.code(200).send(response);
+    },
+  });
+}
--- src/routes/faceit/player/index.ts
+++ src/routes/faceit/player/index.ts
@@ -2,12 +2,17 @@ import type { FastifyInstance } from "fastify";
 
 import type FaceitApiService from "../../../services/faceitApiService/FaceitApiService.js";
 import elo from "./elo.js";
+import getCommand from "./getCommand.js";
 
 export default function player(
   app: FastifyInstance,
   options: { config: { faceitApiService: FaceitApiService } },
 ) {
   app.register((route) => {
-    route.register(elo, { prefix: "/:id/elo", config: options.config });
+    route.register(elo, { prefix: "/elo/:id", config: options.config });
+    route.register(getCommand, {
+      prefix: "/get-command/:id",
+      config: options.config,
+    });
   });
 }
